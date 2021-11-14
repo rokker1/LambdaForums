506698ca1203435d9046b0d5b813943942a40a69
A.E.Zhavoronkov <A.E.Zhavoronkov@urfu.me>
2021-11-14 16:40:48 +0500

    Lesson 4 finished 14.11.2021 16:40

---
+++ LambdaForums/Controllers/ForumController.cs
@@ -0,0 +1,35 @@
+﻿using LambdaForums.Data;
+using LambdaForums.Data.Models;
+using LambdaForums.Models.Forum;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaForums.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            var forums = _forumService.GetAll().
+                Select(forum => new ForumListingModel {
+                    Id = forum.Id,
+                    Name = forum.Title,
+                    Description = forum.Description,
+                });
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+            
+    
+            return View(model);
+        }
+    }
+}
