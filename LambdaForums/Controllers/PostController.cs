619551b5c5c25f64c4165092d952ad5655ba2bec
A.E.Zhavoronkov <A.E.Zhavoronkov@urfu.me>
2021-11-20 14:27:41 +0500

    Lesson 10 is done. 20.11.2021

--- LambdaForums/Controllers/PostController.cs
+++ LambdaForums/Controllers/PostController.cs
@@ -2,18 +2,26 @@
 using LambdaForums.Data.Models;
 using LambdaForums.Models.Post;
 using LambdaForums.Models.Reply;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace LambdaForums.Controllers
 {
     public class PostController : Controller
     {
         private readonly IPost _postService;
-        public PostController(IPost postService)
+        private readonly IForum _forumService;
+        private static UserManager<ApplicationUser> _userManager;
+        public PostController(IPost postService, IForum forumService, UserManager<ApplicationUser> userManager)
         {
             _postService = postService;
+            _forumService = forumService;
+            _userManager = userManager;
+
         }
 
         public IActionResult Index( int id)
@@ -52,6 +60,47 @@ namespace LambdaForums.Controllers
                
             });
 
+        }
+        public IActionResult Create(int id)
+        {
+            var forum = _forumService.GetById(id);
+            var model = new NewPostModel
+            {
+                ForumName = forum.Title,
+                ForumId = forum.Id,
+                ForumImageUrl = forum.ImageUrl,
+                AuthorName = User.Identity.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost(NewPostModel model)
+        {
+
+            var userId = _userManager.GetUserId(User);
+            var user = _userManager.FindByIdAsync(userId).Result;
+            var post = BuildPost(model, user);
+
+            _postService.Add(post).Wait();
+            //TODO: Implement User management system
+
+            return RedirectToAction("Index", "Post", new { id = post.Id });            
+        }
+
+        private Post BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            var forum = _forumService.GetById(model.ForumId);
+            return new Post
+            {
+                Title = model.Title,
+                Content = model.Content,
+                Created = DateTime.Now,
+                User = user,
+                Forum = forum
+
+            };
+        
         }
     }
 }
