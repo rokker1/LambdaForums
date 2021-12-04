b04286f37e5a0325ccddad9a6b8d0862070577e6
A.E.Zhavoronkov <A.E.Zhavoronkov@urfu.me>
2021-12-04 21:19:45 +0500

    lesson 27 is done!

--- LambdaForums/Controllers/ProfileController.cs
+++ LambdaForums/Controllers/ProfileController.cs
@@ -1,8 +1,10 @@
 ﻿using LambdaForums.Data;
 using LambdaForums.Data.Models;
 using LambdaForums.Models.ApplicationUser;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace LambdaForums.Controllers
 {
@@ -22,6 +24,7 @@ namespace LambdaForums.Controllers
         {
             var user = _userService.GetById(id);
             var userRoles = _userManager.GetRolesAsync(user).Result;
+
             var model = new ProfileModel()
             {
                 UserId = user.Id,
@@ -32,7 +35,20 @@ namespace LambdaForums.Controllers
                 ProfilelmageUrl = user.ProfileImageUrl,
                 IsAdmin = userRoles.Contains("Admin")
             };
+
+
+
             return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadProfileImage(IFormFile file)
+        {
+            var userId = _userManager.GetUserId(User);
+            //connect to azure
+            // get nlob container
+            //parse content
+            await 
         }
     }
 }
